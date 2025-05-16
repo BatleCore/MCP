bcd36103566816453910184eb575a66fe5e1869f
XxZelda132xX <mathewreed2006@gmail.com>
2025-05-16 10:34:26 +0800

    bat + LDR

--- src/testing_ground.c
+++ src/testing_ground.c
@@ -1,7 +1,21 @@
-#include "test_battery.h"
+#include "battery.h"
 #include "motor_control.h"
 #include "servo_control.h"
 #include "dist_sensor.h"
+
+void init_everything() {
+  // Provided Libraries
+  adc_init();
+  serial0_init();
+  serial1_init();
+  serial2_init();
+  serial4_init();
+  serial5_init();
+  lcd_init();
+  // Our Libraries
+  motor_init();
+  servo_init();
+}
 
 int main(void) {
   motor_init();
