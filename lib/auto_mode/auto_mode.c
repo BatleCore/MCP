8016ffc0c2ba2c77cec03fbe8ce67f55f71f7293
batlecore <corey.batley@gmail.com>
2025-05-22 16:59:42 +0800

    mode 5 is wonderful

--- lib/auto_mode/auto_mode.c
+++ lib/auto_mode/auto_mode.c
@@ -7,7 +7,7 @@
 uint16_t distance_values[3] = {0}; // initialize all to 0
 uint8_t motor_data[4] = {0};
 int front_hard_thresh = 125; // hard limit - dont go forward
-int front_soft_thresh = 200; // soft limit - begin wide turn
+int front_soft_thresh = 175; // soft limit - begin wide turn
 int side_hard_thresh = 150;
 char msg[50];
 
@@ -139,18 +139,19 @@ void mode_5() {
     motor_turn_forward((distance_values[2]<distance_values[0]));
   } else {
     motor_straight_forward();
+
+    if (distance_values[0] < side_hard_thresh) { 
+      sprintf(msg, "left hard limit\n");
+      serial0_print_string(msg);
+      motor_turn_modifier(1);
+    }
+    if (distance_values[2] < side_hard_thresh) { 
+      sprintf(msg, "right hard limit\n");
+      serial0_print_string(msg);
+      motor_turn_modifier(0);
+    }
   }
 
-  // if (distance_values[0] < side_hard_thresh) { 
-  //   sprintf(msg, "left hard limit\n");
-  //   serial0_print_string(msg);
-  //   motor_turn_modifier(0);
-  // }
-  // if (distance_values[1] < side_hard_thresh) { 
-  //   sprintf(msg, "right hard limit\n");
-  //   serial0_print_string(msg);
-  //   motor_turn_modifier(1);
-  // }
   sprintf(msg, "Speed: %d, %d\nTurning: %d, %d\n", motor_data[0], motor_data[1], motor_data[2], motor_data[3]);
   serial0_print_string(msg);
   rs_motor_conversion(motor_data);
