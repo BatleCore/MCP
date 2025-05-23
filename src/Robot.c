745d329d2801ac2d4701e9d5a6f460f7950c61f4
batlecore <corey.batley@gmail.com>
2025-05-23 09:34:59 +0800

    preparing Robot.c

--- src/Robot.c
+++ src/Robot.c
@@ -1,26 +1,5 @@
 // Lab 8 Robot Micro
 #include "Robot.h"
- 
-// Constants
-#define PACKETSIZE 6  // Max serial packet size we expect
- 
-// H-Bridge & Motor Control
-#define PORT_PWM PORTB
-#define DDR_PWM DDRB
-#define PIN_PWM_ML PB5 // D11 → OC1A (PWM output for left motor)
-#define PIN_PWM_MR PB6 // D12 → OC1B (PWM output for right motor)
-#define PORT_CONTROL PORTA
-#define DDR_CONTROL DDRA
-
-// BATTERY SENSOR
-#define PIN_BATTERY_SENSE PF0
-#define PIN_BATTERY_LED PA4
-#define DDR_BATTERY_LED DDRA
-#define PORT_BATTERY PORTA
-#define PORT_ADC PORTF
-#define DDR_ADC DDRF
-#define BATTERY_THRESH 300
- 
 
 /********************
 Robot Initialization
@@ -31,22 +10,24 @@ Robot Initialization
 ********************/
 void setup() {
   cli();           // Disable interrupts during setup
-  DDR_BATTERY_LED |= (1<<PIN_BATTERY_LED); // 
-  // DDR_ADC &= ~(1<<PIN_BATTERY_SENSE); // battery pin ADC
-
-  PORT_BATTERY &= ~(1<<PIN_BATTERY_LED);
-
+  
+  // communication channels
   serial0_init();      // Debug serial
   serial2_init();      // Xbee serial
 
+  // features
+  battery_init(); 
+  comms_init();
+  display_init();
+  LDR_init();
   motor_init();
   servo_init();
 
   milliseconds_init();   // Millisecond timing
-  // timerPWM_init();     // Timer1 PWM setup
   _delay_ms(20);
   adc_init();        // Analog input (joystick + sensors)
   _delay_ms(20);
+
   sei();           // Enable interrupts
 }
  
@@ -60,26 +41,44 @@ Main Program Loop
 int main(void) {
   setup();
  
-  uint8_t dataRX[PACKETSIZE];
-  int x_val = 0;
-  int y_val = 0;
-  uint8_t motor_d[4];
-  motor_d[0] = 0;
-  motor_d[1] = 0;
-  motor_d[2] = 0;
-  motor_d[3] = 0;
-  int adc_bat_val = 0;
-  uint8_t servo_d[2];
+  uint8_t dataRX[PACKETSIZE]; // move to Robot.h?
+
+  uint8_t servo_d[2]; // to be made obsolete in servo.h
   servo_d[0] = 0;
   servo_d[1] = 0;
 
-  char msg[30];
-  uint32_t lastSend = 0;   // Last time a packet was sent
+  char msg[30]; // for debugging
   
-  
-  sprintf(msg, "SERIAL ACTIVE");
-  serial0_print_string(msg);
   while (1) {
+
+    // switch:case for AUTO and MANUAL modes
+    switch (current_mode)
+    {
+    case AUTO_MODE:
+      while (1) {
+        // read auto serial
+        if ( current_mode != AUTO_MODE ) {
+          break;
+        } else {
+          // do auto mode
+        }
+      }
+      break;
+    
+    case MANUAL_MODE:
+      while (1) {
+        // read auto serial
+        if ( current_mode != MANUAL_MODE ) {
+          break;
+        } else {
+          // do manual mode
+        }
+      }      
+      break;
+
+    default:
+      break;
+    }
 
     // Receive Serial Coms
     if (serial2_available()) { // used when controlling via serial
@@ -89,15 +88,11 @@ int main(void) {
  
         case JOYSTICK_MOTOR_READ: {
           // Use joystick X/Y to update motor control
-          motor_d[0] = dataRX[1];
-          motor_d[1] = dataRX[2];
-          motor_d[2] = dataRX[3];
-          motor_d[3] = dataRX[4];
-          rs_motor_conversion(motor_d);
+          motor_fromSerial(dataRX);
           break;
         }
 
-        case JOYSTICK_SERVO_READ: {
+        case JOYSTICK_SERVO_READ: { // this needs a deep dive to clean
           servo_d[0] = dataRX[1];
           servo_d[1] = dataRX[2];
           servo_set_velocity(servo_d);
@@ -113,20 +108,6 @@ int main(void) {
         }
       }
     }
-
-    // Serial debug at interval
-    if (milliseconds_now() - lastSend >= 500) {
-      lastSend = milliseconds_now();
-      // sprintf(msg, "\nL: %d : %d\n", motor_d[1], motor_d[0]);
-      // serial0_print_string(msg);
-      // sprintf(msg, "R: %d : %d\n", motor_d[3], motor_d[2]);
-      // serial0_print_string(msg);
-      // sprintf(msg, "Battery reading:%d\n", adc_bat_val);
-      // serial0_print_string(msg);
-      sprintf(msg, "\nvel: %d, dir: %d", servo_d[0], servo_d[1]);
-      serial0_print_string(msg);
-    }
-
   }
   return 0;
 }
--- src/Robot.h
+++ src/Robot.h
@@ -16,13 +16,25 @@
 
 //include custom header files
 #include "PIN_ASSIGNMENT.h"
-// #include "communication.h"
+
+#include "battery.h"
+#include "communication.h"
+#include "controller_display.h"
+#include "light_sensor.h"
 #include "motor_control.h"
 #include "servo_control.h"
 
 //constants
 #define BUILD_DATE __TIME__ " " __DATE__"\n"
+#define AUTO_MODE 0
+#define MANUAL_MODE 1
+
+//variables - scope/global
+uint8_t dataRX[PACKETSIZE] = {0};
+int current_mode = MANUAL_MODE;
+
 
 //functions
+void setup();
 
 #endif
